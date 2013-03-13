95e428a971fb5e85f9c658c6104667174fca2c83
Chris Bushell <c.bushell@gmail.com>
2013-03-13 12:55:58 -0700

    use apache commons dbcp

--- src/main/java/foo/bar/baz/SimpleBatchFlow.java
+++ src/main/java/foo/bar/baz/SimpleBatchFlow.java
@@ -1,6 +1,6 @@
 package foo.bar.baz;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.batch.core.JobParameters;
@@ -36,7 +36,9 @@ public class SimpleBatchFlow {
 		simpleBatchFlow.run();
 	}
 
-	public SimpleBatchFlow(JobLauncher jobLauncher, JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+	public SimpleBatchFlow(JobLauncher jobLauncher,
+			JobRepository jobRepository,
+			PlatformTransactionManager transactionManager) {
 		this.jobLauncher = jobLauncher;
 		this.jobRepository = jobRepository;
 		this.transactionManager = transactionManager;
@@ -58,8 +60,14 @@ public class SimpleBatchFlow {
 		taskletStep1.setJobRepository(jobRepository);
 		taskletStep1.setTasklet(tasklet1);
 
-		List<Step> steps = new ArrayList<Step>();
-		steps.add(taskletStep1);
+		Tasklet tasklet2 = new FileCopyTask();
+		TaskletStep taskletStep2 = new TaskletStep("my second step");
+		taskletStep2.setTransactionManager(transactionManager);
+		taskletStep2.setJobRepository(jobRepository);
+		taskletStep2.setTasklet(tasklet2);
+
+		List<Step> steps = Arrays.asList(new Step[] { taskletStep1,
+				taskletStep2 });
 
 		SimpleJob job = new SimpleJob("my first job");
 		job.setJobRepository(jobRepository);
